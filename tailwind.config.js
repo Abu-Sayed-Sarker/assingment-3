dba30eeabd5a07111533da385af422fd74496828
Abu-Sayed-Sarker <abusyed96728@gmail.com>
2024-08-22 18:57:44 +0600

    header section done

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        myclore: "green"
+        full_body: "rgb(244, 244, 244)",
+        gradint_color_1: "rgb(255, 255, 255)",
+        gradint_color_2: "#FFFFFF",
+        textColor: "#212121",
+        praimary_color: "#ABEF5F",
+        secoundery_text_color: "rgb(115, 115, 115)",
+        secoundery_p_color: "rgb(73, 73, 73)",
+        secound_btn_color: "rgba(171, 239, 95, 0.5)"
+      },
+      fontFamily: {
+        Manrope: "Manrope, sans-serif;"
       }
     },
   },
